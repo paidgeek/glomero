fb113fdf8941b235f06dfd957eba6c510b4d3c5d
paidgeek <tin.rabzelj@gmail.com>
2016-01-31 09:48:36 +0100

    level generator with markov chains

--- Glomero/SectionFactory.m
+++ Glomero/SectionFactory.m
@@ -1,29 +1,82 @@
 #import "SectionFactory.h"
 #import "TINR.Glomero.h"
 
+#define NUM_STATES 2
+
+typedef enum {
+	StateEmpty,
+	StateTiled
+} State;
+
+@interface SectionFactory ()
+- (void) createEmpty:(Node *) node;
+- (void) createTiled:(Node *) node;
+@end
+
 @implementation SectionFactory {
+	Glomero *glomero;
+	Scene *scene;
+	
 	float z;
+	float chain[NUM_STATES][NUM_STATES];
+	State state;
 }
 
 - (id) init {
 	self = [super init];
 	
 	if(self) {
+		glomero = [Glomero getInstance];
+		scene = [Scene getInstance];
+		
 		z = 0.0f;
+		
+		chain[StateEmpty][StateEmpty] = 0.5f;
+		chain[StateEmpty][StateTiled] = 0.5f;
+		
+		chain[StateTiled][StateEmpty] = 0.4f;
+		chain[StateTiled][StateTiled] = 0.6f;
+		
+		state = StateEmpty;
 	}
 	
 	return self;
 }
 
 - (Node *) create {
-	Scene *scene = [Scene getInstance];
-	Glomero *glomero = [Glomero getInstance];
-	
 	Node *node = [scene createNode];
 	node.transform.position = [Vector3 vectorWithX:0.0f y:-0.5f z:z - SECTION_LENGTH / 2.0f];
 	
+	switch (state) {
+		case StateEmpty:
+			[self createEmpty:node];
+			break;
+		case StateTiled:
+			[self createTiled:node];
+			break;
+	}
+	
+	z -= SECTION_LENGTH;
+	
+	float r = [Random float];
+	float sum = 0.0f;
+	
+	for(int i = 0; i < NUM_STATES; i++) {
+		sum += chain[state][i];
+		
+		if(r <= sum) {
+			state = i;
+			
+			break;
+		}
+	}
+	
+	return node;
+}
+
+-(void)createEmpty:(Node *)node {
 	MeshRenderer *mr = [node addComponentOfClass:[MeshRenderer class]];
-	mr.effect = [Random intLessThan:2] == 0 ? glomero.platformEffect0 : glomero.platformEffect1;
+	mr.effect = glomero.platformEffect0;
 	mr.mesh = [MeshFactory createCubeWithGraphicsDevice:scene.game.graphicsDevice
 																 width:3
 																height:1
@@ -31,7 +84,19 @@
 	AABoxCollider *collider = [node addComponentOfClass:[AABoxCollider class]];
 	collider.min = [Vector3 vectorWithX:-1.5f y:-0.5f z:-SECTION_LENGTH / 2.0f];
 	collider.max = [Vector3 vectorWithX:1.5f y:0.5f z:SECTION_LENGTH / 2.0f];
-	/*
+}
+
+-(void)createTiled:(Node *)node {
+	MeshRenderer *mr = [node addComponentOfClass:[MeshRenderer class]];
+	mr.effect = glomero.platformEffect0;
+	mr.mesh = [MeshFactory createCubeWithGraphicsDevice:scene.game.graphicsDevice
+																 width:3
+																height:1
+																 depth:SECTION_LENGTH];
+	AABoxCollider *collider = [node addComponentOfClass:[AABoxCollider class]];
+	collider.min = [Vector3 vectorWithX:-1.5f y:-0.5f z:-SECTION_LENGTH / 2.0f];
+	collider.max = [Vector3 vectorWithX:1.5f y:0.5f z:SECTION_LENGTH / 2.0f];
+	
 	for(int i = 0; i < 5; i++) {
 		float x = [Random intGreaterThanOrEqual:-2 lessThan:2] + 0.5f;
 		float z = [Random intLessThan:SECTION_LENGTH] + 0.5f;
@@ -43,11 +108,6 @@
 		mr.effect = glomero.platformEffect1;
 		mr.mesh = [MeshFactory createCubeWithGraphicsDevice:scene.game.graphicsDevice width:1 height:1 depth:1];
 	}
-	*/
-	
-	z -= SECTION_LENGTH;
-	
-	return node;
 }
 
 @end
